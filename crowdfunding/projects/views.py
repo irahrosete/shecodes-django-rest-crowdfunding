b7a90998fafea24118f5821fe936f3a899af94e0
irah rosete <irah.rosete@outlook.com>
2023-01-16 00:18:10 +1100

    update status codes

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 from .models import Project
 from .serializers import ProjectSerializer
 
@@ -13,11 +15,21 @@ class ProjectList(APIView):
     serializer = ProjectSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return Response(serializer.data)
+      return Response(
+        serializer.data,
+        status=status.HTTP_201_CREATED,
+      )
+    return Response(
+      serializer.errors,
+      status=status.HTTP_400_BAD_REQUEST,
+    )
 
 class ProjectDetail(APIView):
   def get_object(self, pk):
-    return Project.objects.get(pk=pk)
+    try:
+      return Project.objects.get(pk=pk)
+    except Project.DoesNotExist:
+      raise Http404
 
   def get(self, request, pk):
     project = self.get_object(pk)
