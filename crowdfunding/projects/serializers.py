ab76b104f06c2021a07f0bc05a94c9ca3beca240
irah rosete <irah.rosete@outlook.com>
2023-01-16 00:42:47 +1100

    show all pledges for a project

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,19 +1,6 @@
 from rest_framework import serializers
 from .models import Project, Pledge
 
-class ProjectSerializer(serializers.Serializer):
-  id = serializers.ReadOnlyField()
-  title = serializers.CharField(max_length=200)
-  description = serializers.CharField(max_length=None)
-  goal = serializers.IntegerField()
-  image = serializers.URLField()
-  is_open = serializers.BooleanField()
-  date_created = serializers.DateTimeField(read_only=True)
-  owner = serializers.CharField(max_length=200)
-
-  def create(self, validated_data):
-    return Project.objects.create(**validated_data)
-
 class PledgeSerializer(serializers.Serializer):
   id = serializers.ReadOnlyField()
   amount = serializers.IntegerField()
@@ -24,3 +11,17 @@ class PledgeSerializer(serializers.Serializer):
 
   def create(self, validated_data):
     return Pledge.objects.create(**validated_data)
+
+class ProjectSerializer(serializers.Serializer):
+  id = serializers.ReadOnlyField()
+  title = serializers.CharField(max_length=200)
+  description = serializers.CharField(max_length=None)
+  goal = serializers.IntegerField()
+  image = serializers.URLField()
+  is_open = serializers.BooleanField()
+  date_created = serializers.DateTimeField(read_only=True)
+  owner = serializers.CharField(max_length=200)
+  pledges = PledgeSerializer(many=True, read_only=True)
+
+  def create(self, validated_data):
+    return Project.objects.create(**validated_data)
