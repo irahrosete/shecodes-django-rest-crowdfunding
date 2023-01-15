5f83282d470e0aaa25e6001b1e9687bf754d01b5
irah rosete <irah.rosete@outlook.com>
2023-01-16 01:30:31 +1100

    add login function and link user to project as owner

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
     path('', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
