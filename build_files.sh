bfadf501c3463d5022b4b8c44e823622080f3c18
irah rosete <irah.rosete@outlook.com>
2023-01-29 03:39:13 +1100

    add vercel deploy config

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
 # build_files.sh
-pip install --upgrade pip
-python -m venv .venv
-. .venv/bin/activate
-pip install -r requirements.txt
-python /crowdfunding/manage.py collectstatic --noinput
+# pip install --upgrade pip
+# python -m venv .venv
+# . .venv/bin/activate
+# pip install -r requirements.txt
+# python /crowdfunding/manage.py collectstatic --noinput
+
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
