f4bae7f1e07ca6dc2e061a16575785bf7dc9ace8
irah rosete <irah.rosete@outlook.com>
2023-01-29 02:51:50 +1100

    add vercel deploy config

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,4 @@ pip install --upgrade pip
 python3.9 -m venv .venv
 . .venv/bin/activate
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.9 crowdfunding/manage.py collectstatic
