0117b54b3e68cf9d13a5f06ab1aa31bd51129ea8
irah rosete <irah.rosete@outlook.com>
2023-01-29 03:48:05 +1100

    add vercel deploy config

--- build_files.sh
+++ build_files.sh
@@ -11,8 +11,8 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.9 /crowdfunding/manage.py makemigrations --noinput
+python3.9 /crowdfunding/manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 /crowdfunding/manage.py collectstatic --noinput --clear
