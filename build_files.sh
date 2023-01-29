5b67e2182ca905904dbad039c74e20eed0f3e287
irah rosete <irah.rosete@outlook.com>
2023-01-29 12:42:05 +1100

    add vercel deploy config

--- build_files.sh
+++ build_files.sh
@@ -7,12 +7,17 @@
 # pip install -r requirements.txt
 # python /crowdfunding/manage.py collectstatic --noinput
 
+
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 # echo "Make Migration..."
 # python3.9 /crowdfunding/manage.py makemigrations --noinput
-# python3.9 /crowdfunding/manage.py migrate --noinput
+python3.9 /crowdfunding/manage.py migrate --noinput
+python3.9 /crowdfunding/manage.py createsuperuser --noinput
+
+
+gunicorn --bind :8000 --workers 1 crowdfunding.wsgi
 
 echo "Collect Static..."
 python3.9 /crowdfunding/manage.py collectstatic --noinput --clear
